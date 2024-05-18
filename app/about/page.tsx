ff6a75a53db9385456a96206952d817182668b9c
vasanth1066 <vasanthanuvavi@gmail.com>
2024-05-18 20:00:31 +0530

    Why do we need to learn Next JS

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function about() {
+    return (
+      <main>
+       <h1> Home page</h1>
+      </main>
+    );
+  }
+  
---
+++ app/news/page.js
@@ -0,0 +1,7 @@
+export default function news() {
+  return (
+    <main>
+      <h1> News page</h1>
+    </main>
+  );
+}
