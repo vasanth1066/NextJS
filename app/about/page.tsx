7fc3af76a5d2339fb081f24118e2d738e0a86d3a
vasanth1066 <vasanthanuvavi@gmail.com>
2024-05-24 13:47:46 +0530

    Final touch to About US page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+// app/aboutus/page.tsx
+"use client";
 
-export default function about() {
-    return (
-      <main>
-       <h1> About page</h1>
-      </main>
-    );
-  }
-  
+import Link from 'next/link';
+
+const details = [
+  { id: 1, name: 'Yash', role: 'Senior Developer' },
+  { id: 2, name: 'Vaibhav', role: 'Backend Developer' },
+  { id: 3, name: 'Suresh', role: 'Frontend Developer' }
+];
+
+const AboutUsPage = () => {
+  return (
+    <div>
+      <h1>Developers</h1>
+      <ul>
+        {details.map((developer) => (
+          <li key={developer.id}>
+          <Link href={`/about/${developer.id}`}>{developer.name}</Link>
+
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default AboutUsPage;
